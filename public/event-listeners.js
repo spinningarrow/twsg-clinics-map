9e5e8cfdee3fab428e2d1e8f01143016ef6982d4
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-01-06 11:54:12 +0800

    Remove old files

--- public/event-listeners.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#saturdays').addEventListener('click', showClinicsOpenOnSaturdays)
-document.querySelector('#sundays').addEventListener('click', showClinicsOpenOnSundays)
-document.querySelector('#public-holidays').addEventListener('click', showClinicsOpenOnPublicHolidays)
-document.querySelector('#twenty-four').addEventListener('click', showClinicsOpen24Hours)
