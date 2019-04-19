4d8506c461f5caed0de4bf7acda2e439570be2c0
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2019-04-19 18:25:31 +0200

    Inline variables

--- cleaner/normalise_values.clj
+++ cleaner/normalise_values.clj
@@ -5,17 +5,15 @@
 
 (defn normalise-closed
   [value]
-  (let [string (str value)]
-    (if-let [[_ match] (re-find #"(?i)\s*(closed)\s*" string)]
-      (capitalize match)
-      value)))
+  (if-let [[_ match] (re-find #"(?i)\s*(closed)\s*" (str value))]
+    (capitalize match)
+    value))
 
 (defn normalise-24-hours
   [value]
-  (let [string (str value)]
-    (if (re-find #"(?i)\s*24 hours\s*" string)
-      "24 Hours"
-      value)))
+  (if (re-find #"(?i)\s*24 hours\s*" (str value))
+    "24 Hours"
+    value))
 
 (defn transform-map
   [m]
